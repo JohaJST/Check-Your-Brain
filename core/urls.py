86e499ad1ba17e1cf712706800d67ec285c68547
spacecraft <sarvarabduquddusov2@gmail.com>
2024-01-19 21:10:33 +0500

    CreateTest

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from core.views import index
+from core.views import index, new_test, create_test
 from core.auth import sign_in, regis
 
 urlpatterns = [
     path("", index, name="home"),
     path("login", sign_in, name="login"),
     path("regis/", regis, name="regis"),
+    path("test/new/", new_test, name="new_test"),
+    path("test/create/", create_test, name="create_test")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import *
+
 
 @login_required(login_url="login")
 def index(request):
     return render(request, 'index.html')
+
+
+def new_test(request):
+    class_rooms = ClassRooms.objects.all()
+    subjects = Subject.objects.all()
+    return render(request, "new.html", {"classrooms": class_rooms, "subjects": subjects})
+
+
+def create_test(request):
+    print("1")
+    if request.method == "POST":
+        data = request.POST['text']
+        print(data)
+    return redirect("/")
