20fda7c9cb48628e54102c0900aa2942256c2ad0
Galaxy-craft <sarvarabduquddusov2@gmail.com>
2024-02-05 08:03:27 +0500

    .

--- core/dashboard/action.py
+++ core/dashboard/action.py
@@ -9,6 +9,15 @@ def action(request, status, pk, path):
                 t = Test.objects.filter(pk=pk).first()
                 t.is_start = True
                 t.save()
+                return redirect("dlist", tip=path)
+            except:
+                return redirect("dlist", tip=path)
+        elif status == "end":
+            try:
+                t = Test.objects.filter(pk=pk).first()
+                t.is_start = False
+                t.save()
+                return redirect("dlist", tip=path)
             except:
                 return redirect("dlist", tip=path)
         else:
