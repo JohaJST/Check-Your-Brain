36f9816e98cee84adc2b8ca4848535668b8687c6
Joha-JST <154612609+JohaJST@users.noreply.github.com>
2024-02-06 15:54:08 +0500

    update

--- core/dashboard/action.py
+++ core/dashboard/action.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from core.models import Test
+from core.models import Test, ClassRooms, Subject
 
 
 def action(request, status, pk, path):
@@ -20,7 +20,30 @@ def action(request, status, pk, path):
                 return redirect("dlist", tip=path)
             except:
                 return redirect("dlist", tip=path)
+        elif status == "create":
+            if path == "test":
+                if request.method == "GET":
+                    is_subject = request.GET.get("is_subject")
+                    class_rooms = ClassRooms.objects.all()
+                    subjects = Subject.objects.all()
+                    return render(request, "pages/dashboard/new.html",
+                                  {"classrooms": class_rooms, "subjects": subjects, "action": "test"})
+                elif request.method == "POST":
+                    return redirect()
+            elif path == "subject":
+                if request.method == "GET":
+                    return render(request, "pages/dashboard/new.html", {"action": "subject"})
+            elif path == "classroom":
+                if request.method == "GET":
+                    return render(request, "pages/dashboard/new.html", {"action": "classroom"})
+            # elif path == "classroom_subject":
+            #     if request.method == "GET":
+            #         return render(request, "pages/dashboard/new.html", {"action": "classroom_subject"})
+            return redirect("dlist", tip=path)
         else:
             return redirect("dlist", tip=path)
     else:
         return redirect("home")
+
+
+
--- core/dashboard/home.py
+++ core/dashboard/home.py
@@ -1,11 +1,18 @@
 from django.shortcuts import redirect, render
 
-from core.models import Test
+from core.models import Test, Result, User, Subject
 
 
 def home(requests):
     if requests.user.is_admin:
         q = Test.objects.all()
-        return render(requests, 'pages/dashboard/index.html', {"qlen": len(q)})
+        r = Result.objects.all()
+        u = User.objects.all()
+        s = Subject.objects.all()
+        a = 0
+        for i in r:
+            a += int(i.foyiz)
+        tmp = a // len(r)
+        return render(requests, 'pages/dashboard/index.html', {"qlen": len(q), "rlen": tmp, "ulen": len(u), "slen": len(s)})
     else:
         return redirect("home")
