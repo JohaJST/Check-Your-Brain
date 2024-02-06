8c8f3208fd547cf593e4e11770825f9d64eb1b49
Galaxy-craft <sarvarabduquddusov2@gmail.com>
2024-02-06 21:39:33 +0500

    .

--- core/dashboard/action.py
+++ core/dashboard/action.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from core.models import Test, ClassRooms, Subject
+from core.models import Test, ClassRooms, Subject, ClassRoomsSubjects
 
 
-def action(request, status, pk, path):
+def action(request, status, path, pk=None):
     if request.user.is_admin:
         if status == "start":
             try:
@@ -24,21 +24,32 @@ def action(request, status, pk, path):
             if path == "test":
                 if request.method == "GET":
                     is_subject = request.GET.get("is_subject")
-                    class_rooms = ClassRooms.objects.all()
+                    classrooms = ClassRooms.objects.all()
                     subjects = Subject.objects.all()
                     return render(request, "pages/dashboard/new.html",
-                                  {"classrooms": class_rooms, "subjects": subjects, "action": "test"})
+                                  {"classrooms": classrooms, "subjects": subjects, "action": "test"})
                 elif request.method == "POST":
-                    return redirect()
+                    return redirect("/dashboard/")
             elif path == "subject":
                 if request.method == "GET":
-                    return render(request, "pages/dashboard/new.html", {"action": "subject"})
+                    classrooms = ClassRooms.objects.all()
+                    return render(request, "pages/dashboard/new.html", {"action": "subject", "classrooms": classrooms})
+                elif request.method == "POST":
+                    subject = Subject.objects.create(name=request.POST.get("subject_name"))
+                    subject.save()
+                    classroom_id = 0
+                    while f'classroom_{classroom_id}' in request.POST:
+                        clsb = ClassRoomsSubjects.objects.create(classroom_id=ClassRooms.objects.get(id=request.POST.get(f'classroom_{classroom_id}')).id, subject_id=subject.id)
+                        clsb.save()
+                        classroom_id += 1
+                    return redirect("/dashboard/list/subject/")
             elif path == "classroom":
                 if request.method == "GET":
                     return render(request, "pages/dashboard/new.html", {"action": "classroom"})
-            # elif path == "classroom_subject":
-            #     if request.method == "GET":
-            #         return render(request, "pages/dashboard/new.html", {"action": "classroom_subject"})
+                elif request.method == "POST":
+                    class_room = ClassRooms.objects.create(name=request.POST.get("classroom_name"))
+                    class_room.save()
+                    return redirect("/dashboard/list/classroom/")
             return redirect("dlist", tip=path)
         else:
             return redirect("dlist", tip=path)
