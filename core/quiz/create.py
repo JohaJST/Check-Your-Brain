cb1b7ee44db61a5b7048f0af22604b6691998062
Galaxy-craft <sarvarabduquddusov2@gmail.com>
2024-02-07 10:58:12 +0500

    .

--- core/quiz/create.py
+++ core/quiz/create.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from core.models import ClassRooms, Subject, Question, Variant, Test
+from core.models import ClassRooms, Subject, Question, Variant, Test, TestClassRoom
 
 
 def new_test(request):
@@ -11,14 +11,23 @@ def new_test(request):
 
 def create_test(request):
     if request.method == 'POST':
-        classroom = ClassRooms.objects.get(id=request.POST.get('classroom'))
         subject = Subject.objects.get(id=request.POST.get('subject'))
-        test_name = Test.objects.create(name=request.POST.get('test_name'), classroom=classroom, subject=subject)
+        test_name = Test.objects.create(name=request.POST.get('test_name'), subject=subject)
         question_counter = 1
+        try:
+            tscr = TestClassRoom.objects.create(test_id=test_name.id, classroom_id=ClassRooms.objects.get(id=request.POST.get('classroom_1')).id)
+            tscr.create()
+            tscr = TestClassRoom.objects.create(test_id=test_name.id, classroom_id=ClassRooms.objects.get(id=request.POST.get('classroom_2')).id)
+            tscr.create()
+            tscr = TestClassRoom.objects.create(test_id=test_name.id, classroom_id=ClassRooms.objects.get(id=request.POST.get('classroom_3')).id)
+            tscr.create()
+            tscr = TestClassRoom.objects.create(test_id=test_name.id, classroom_id=ClassRooms.objects.get(id=request.POST.get('classroom_4')).id)
+            tscr.create()
+        except:
+            print(123)
         while f'question_{question_counter}' in request.POST:
             question_text = request.POST[f'question_{question_counter}']
             question = Question.objects.create(text=question_text, test=test_name)
-
             variant_counter = 1
             while f'variant_{question_counter}_{variant_counter}' in request.POST:
                 variant_text = request.POST[f'variant_{question_counter}_{variant_counter}']
