a16dcb585a8f2f2457db835ec7b1da3ee6c08960
SpaceCraft <sarvarabduquddusov2@gmail.com>
2026-05-13 22:28:29 +0500

    .

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,37 @@
 from contextlib import closing
 
-from import_export.admin import ImportExportModelAdmin
-from .resource import UserResource, VariantResource, ResultResource, SUbjectResource, TestResource, OldResulrResource, \
-    QuestionResource, CLassroomResource, TG_UserResource, TestClassRoomResource, ClassRoomsSubjectsResource
-
 from django.contrib import admin
 from django.db import connection
+from import_export.admin import ImportExportModelAdmin
 from methodism import dictfetchone
 
-from core.models import TG_User, User, ClassRooms, Subject, Variant, Question, Test, Result, ClassRoomsSubjects, \
-    TestClassRoom, OldResult
+from core.models import (
+    ClassRooms,
+    ClassRoomsSubjects,
+    OldResult,
+    Question,
+    Result,
+    Subject,
+    Test,
+    TestClassRoom,
+    TG_User,
+    User,
+    Variant,
+)
+
+from .resource import (
+    CLassroomResource,
+    ClassRoomsSubjectsResource,
+    OldResulrResource,
+    QuestionResource,
+    ResultResource,
+    SUbjectResource,
+    TestClassRoomResource,
+    TestResource,
+    TG_UserResource,
+    UserResource,
+    VariantResource,
+)
 
 
 class UserAdmin(ImportExportModelAdmin):
@@ -71,15 +93,15 @@ admin.site.register(ClassRoomsSubjects, ClassRoomSubjectAdmin)
 
 def userJust():
     c = f"""
-            SELECT * FROM core_user a 
+            SELECT * FROM core_user a
             WHERE a.username = "JustUsername"
         """
     with closing(connection.cursor()) as cursor:
         cursor.execute(c)
         check = dictfetchone(cursor)
     if not check:
-        s = """INSERT INTO core_user (username, password, just, ut, is_active, is_admin, is_staff, is_superuser)
-                VALUES ("JustUsername", "pbkdf2_sha256$600000$v3HUU7hiufzCi58elsVhKG$LvJMwls9/+RLNFyStjZYGGdIZ+9DvnuYT5GYINVse5M=", TRUE, 1, TRUE, TRUE, TRUE, TRUE)"""
+        s = """INSERT INTO core_user (username, password, just, role, is_active, in_dashboard)
+                VALUES ("JustUsername", "pbkdf2_sha256$600000$v3HUU7hiufzCi58elsVhKG$LvJMwls9/+RLNFyStjZYGGdIZ+9DvnuYT5GYINVse5M=", TRUE, 1, TRUE, 0)"""
         with closing(connection.cursor()) as cursor:
             cursor.execute(s)
             dictfetchone(cursor)
